fb40bd2285925bbfd67080c2c7567280da7513e2
Ben Gorski <bengorski@Bens-MacBook-Pro.local>
2022-07-12 10:05:00 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'my-app-5',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
